19056ef1befaf3c43111f8aa84ffb1aad267d4b1
Ned Albo <ned.albo@mailfence.com>
2024-08-15 15:20:45 +0200

    added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,26 @@ const robotoFont = Roboto({
 export const metadata: Metadata = {
     title: "Coinflakes Vault v2",
     description: "Coinflakes Investment Vault",
+    icons: [
+        { rel: "icon", url: "/favicon.ico" },
+        {
+            rel: "apple-touch-icon",
+            url: "/apple-touch-icon.png",
+            sizes: "180x180",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "32x32",
+            url: "/favicon-32x32.png",
+        },
+        {
+            rel: "icon",
+            type: "image/png",
+            sizes: "16x16",
+            url: "/favicon-16x16.png",
+        },
+    ],
 };
 
 export default function RootLayout(props: { children: ReactNode }) {
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,6 +1,6 @@
 import { http, cookieStorage, createConfig, createStorage } from "wagmi";
-import { mainnet, sepolia, localhost } from "wagmi/chains";
-import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
+import { mainnet, localhost } from "wagmi/chains";
+import { coinbaseWallet, injected } from "wagmi/connectors";
 
 export function getConfig() {
     return createConfig({
